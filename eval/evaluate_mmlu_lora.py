f73a2aa1018673695848744ca74989353b5a637b
instromica <45325713+instromica@users.noreply.github.com>
2023-07-26 19:23:42 +0800

    add entrophy

--- eval/evaluate_mmlu_lora.py
+++ eval/evaluate_mmlu_lora.py
@@ -5,6 +5,7 @@ import os
 import sys
 sys.path.append("..")
 import torch
+import torch.nn.functional as F
 import numpy as np
 import pandas as pd
 from categories import subcategories, categories
@@ -12,6 +13,7 @@ from transformers import AutoTokenizer,AutoModelForCausalLM,LlamaForCausalLM, Ll
 from mypeft import PeftModel
 import time
 choices = ["A", "B", "C", "D"]
+all_bins = [] 
 
 def estimate_accs_of_bins_ranked_by_unc(is_correct, uncertainties, num_bins=10):
     is_correct = torch.Tensor(is_correct)
@@ -57,18 +59,15 @@ def eval(args, subject, model, tokenizer, dev_df, test_df):
     all_probs = []
     answers = choices[: test_df.shape[1] - 2]
     uncertainties = []
+    all_entrophies = []
 
     for i in range(test_df.shape[0]):
         # get prompt and make sure it fits
         k = args.ntrain
         prompt_end = format_example(test_df, i, include_answer=False)
-        print(prompt_end)
-        print("*****************************")
-        # exit()
+        # print(prompt_end)
         train_prompt = gen_prompt(dev_df, subject, k)
         prompt = train_prompt + prompt_end
-        # print(prompt)
-        # print("***************************")
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids.cuda()
         while input_ids.shape[-1] > 2048:
             k -= 1
@@ -77,50 +76,73 @@ def eval(args, subject, model, tokenizer, dev_df, test_df):
             input_ids = tokenizer(prompt, return_tensors="pt").input_ids.cuda()
 
         label = test_df.iloc[i, test_df.shape[1] - 1]
-        print("label",label)
-
+        # print("label",label)
 
         is_evaluation = True
-        output, uncertainty = model(
+        # print(model)
+        output, all_logits = model(
             input_ids=input_ids,
             is_generation_mode=True,
+            is_evaluation=True,
         )
-        print("output.logits.shape",output.logits.shape)
+        # print("output.logits.shape",output.logits.shape)
         logits = output.logits[:,-1].flatten()
-        # print("uncertainty.shape",uncertainty.shape)
-        # print("uncertainty",uncertainty)
-        uncertainties.append(uncertainty.item())
 
-        probs = (
-            torch.nn.functional.softmax(
-                torch.tensor(
+        logits = logits.float()
+        entrophies = []
+        probilities = []
+        for each_logit in all_logits:
+            each_logit = each_logit[:,-1].flatten().float()
+            # print("each_logit[tokenizer(A).input_ids[-1]], shape",each_logit[tokenizer("A").input_ids[-1]].shape)
+            abcd = torch.tensor(
                     [
-                        logits[tokenizer("A").input_ids[-1]],
-                        logits[tokenizer("B").input_ids[-1]],
-                        logits[tokenizer("C").input_ids[-1]],
-                        logits[tokenizer("D").input_ids[-1]],
+                        each_logit[tokenizer("A").input_ids[-1]],
+                        each_logit[tokenizer("B").input_ids[-1]],
+                        each_logit[tokenizer("C").input_ids[-1]],
+                        each_logit[tokenizer("D").input_ids[-1]],
                     ]
-                ),
-                dim=0,
-            )
-            .detach()
-            .cpu()
-            .to(torch.float32)
-            .numpy()
-        )
-        print("probs",probs)
+                )
+            # print(abcd)
+            # print("abcd.shape",abcd.shape)
+            entrophy = F.softmax(abcd[:],dim=-1) * F.log_softmax(abcd[:], dim=-1)
+            entrophy = -1.0 * entrophy.sum(-1).squeeze()
+            entrophies.append(entrophy)
+            # print("F.softmax(abcd[:],dim=-1)",F.softmax(abcd[:],dim=-1))
+            probilities.append(F.softmax(abcd[:],dim=-1))
+        # print("entrophies",entrophies)
+        average_of_entrophy = sum(entrophies) / len(entrophies)
+        # print("average_of_entrophy shape",average_of_entrophy.shape)
+        for i in range(len(probilities)):
+            if i == 0:
+                tmp_probs = probilities[i]
+            else:
+                tmp_probs += probilities[i]
+        tmp_probs = tmp_probs/len(probilities)
+        probs = tmp_probs
+        entrophy_of_average = tmp_probs * torch.log(tmp_probs.clamp(min=1e-6))
+        entrophy_of_average = -1.0 * entrophy_of_average.sum(-1).squeeze()
+        # print("average_of_entrophy",average_of_entrophy)
+        # print("entrophy_of_average",entrophy_of_average)
+        uncertainty = entrophy_of_average - average_of_entrophy
+        uncertainties.append(uncertainty.item())
+        all_entrophies.append(entrophy_of_average.item())
+        probs = probs.detach().cpu().to(torch.float32).numpy()
+        # print("probs",probs)
         pred = {0: "A", 1: "B", 2: "C", 3: "D"}[np.argmax(probs)]
 
         cor = pred == label
         cors.append(cor)
         all_probs.append(probs)
-        exit()
+        # exit()
 
     acc = np.mean(cors)
     # print(cors)
     # print(uncertainties)
-    accs_of_bins_ranked_by_unc = estimate_accs_of_bins_ranked_by_unc(cors, uncertainties)
-    print(accs_of_bins_ranked_by_unc)
+    # accs_of_bins_ranked_by_unc = estimate_accs_of_bins_ranked_by_unc(cors, uncertainties)
+    accs_of_bins_ranked_by_entrophy = estimate_accs_of_bins_ranked_by_unc(cors, all_entrophies)
+    # all_bins.append(accs_of_bins_ranked_by_unc)
+    all_bins.append(accs_of_bins_ranked_by_entrophy)
+    print(accs_of_bins_ranked_by_entrophy)
     cors = np.array(cors)
     all_probs = np.array(all_probs)
     print("Average accuracy {:.3f} - {}".format(acc, subject))
@@ -146,8 +168,9 @@ model = LlamaForCausalLM.from_pretrained(
 
 def main(args):
 
-    tokenizer = LlamaTokenizer.from_pretrained(args.model,use_fast=False,add_bos_token=False, model_max_length=4096,padding_side="right")
-    model = LlamaForCausalLM.from_pretrained(args.model,  torch_dtype=torch.bfloat16, device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(args.model,use_fast=False,add_bos_token=False, model_max_length=4096,padding_side="right")
+    print(args.model)
+    model = AutoModelForCausalLM.from_pretrained(args.model,load_in_8bit=True, torch_dtype=torch.bfloat16, device_map="auto")
     model = PeftModel.from_pretrained(
             model,
             args.lora_weight,
@@ -211,6 +234,15 @@ def main(args):
         print("Average accuracy {:.3f} - {}".format(cat_acc, cat))
     weighted_acc = np.mean(np.concatenate(all_cors))
     print("Average accuracy: {:.3f}".format(weighted_acc))
+    print(all_bins)
+
+    file = open('entrophy_res_lora.txt','a')
+    for i in range(len(all_bins)):
+        s = str(all_bins[i]).replace('[','').replace(']','')
+        s = s.replace("'",'').replace(',','') +'\n'  
+        file.write(s)
+    file.close()
+
 
 
 if __name__ == "__main__":
@@ -219,7 +251,7 @@ if __name__ == "__main__":
     parser.add_argument("--ngpu", "-g", type=int, default=8)
     parser.add_argument("--data_dir", "-d", type=str, default="/home/myb/lora_llama/data")
     parser.add_argument("--save_dir", "-s", type=str, default="results")
-    parser.add_argument("--lora_weight", "-l",type=str,  default="/home/myb/pyllama_data/yhamam1v2_0710") #yhamam1v5_0716_r16 #yhamam1v2_0710
+    parser.add_argument("--lora_weight", "-l",type=str,  default="/home/myb/pyllama_data/yhamam1v2_0722_r16") #yhamam1v5_0716_r16 #yhamam1v2_0710
     parser.add_argument(
         "--model",
         "-m",
